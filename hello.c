370c4cf2bcc0b33c783fe61dfd86c35f60eed0f4
pdahd <yswwsy@outlook.com>
2024-05-09 14:06:44 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
+
 int main() {
-  printf("你好吗！\n");
-  return 0;
+    const char *poem = "床前明月光，\n疑是地上霜。\n举头望明月，\n低头思故乡。\n";
+    const char *p = poem;
+    while (*p) {
+        printf("%c", *p++);
+        fflush(stdout); // 确保每个字符都立即打印
+        usleep(200000); // 等待200毫秒
+    }
+    return 0;
 }
