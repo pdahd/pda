adcebe4f94e976ad0bc72549ba138bce96af5ed3
Terry <168416857+pdahd@users.noreply.github.com>
2024-11-20 16:23:33 +0800

    调整止步

--- loading-animation/worker.js
+++ loading-animation/worker.js
@@ -132,11 +132,11 @@ async function handleRequest(request) {
                 align-items: center;
                 justify-content: center;
                 height: 21px; 
-                background: rgba(50, 50, 50, 0.3); 
+                background: rgba(50, 50, 50, 0.4); 
                 padding: 3px; /* 微调内边距 */
                 border-radius: 6px; /* 圆角背景 */
                 border: 2px solid rgba(245, 245, 245, 0.5);
-                box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.8);
+                box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.6);
             }
             /* 项目标题 MapMyIP */
             #projectName {
@@ -145,14 +145,14 @@ async function handleRequest(request) {
                 color: #f5f5f5; 
                 text-stroke: 1px #f5f5f5;
                 -webkit-text-stroke: 1px #f5f5f5; 
-                text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
+                text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.6);
             }
             /* 副标题 你的IP地图 */
             #projectSubtitle {
                 font-size: 15px;
                 font-family: sans-serif;
                 color: #f5f5f5; 
-                text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
+                text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.6);
                 margin-left: 5px; 
             }
         </style>
