929dbd773e30ecb843a1ead06aa5137ec781c20c
Terry <168416857+pdahd@users.noreply.github.com>
2024-11-20 11:45:32 +0800

    Update worker.js

--- loading-animation/worker.js
+++ loading-animation/worker.js
@@ -134,8 +134,8 @@ async function handleRequest(request) {
                 height: 30px; /* 设置容器的高度 */
                 line-height: 30px; /* 确保上下居中 */
                 background: rgba(50, 50, 50, 0.7); /* 半透明深灰色背景 */
-                padding: 3px 3px; /* 微调内边距 */
-                border-radius: 8px; /* 圆角背景 */
+                padding: 3px; /* 微调内边距 */
+                border-radius: 5px; /* 圆角背景 */
                 border: 2px solid rgba(255, 255, 255, 0.3);
                 box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.8), inset 1px 1px 4px rgba(255, 255, 255, 0.1);
                 box-sizing: border-box;
