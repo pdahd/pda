16ee06bef633de3cff739e9fafdc209c918b8fad
Terry <168416857+pdahd@users.noreply.github.com>
2024-11-26 11:45:31 +0800

    Update worker.js

--- loading-animation/worker.js
+++ loading-animation/worker.js
@@ -1,5 +1,3 @@
-/* global HTML_CONTENT */
-
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
@@ -9,7 +7,7 @@ async function handleRequest(request) {
 
   // 如果是GET请求，返回index.html
   if (method === 'GET') {
-    const htmlContent = await HTML_CONTENT.get('index.html');
+    const htmlContent = await HTML_STORAGE.get('index.html');
     if (!htmlContent) {
       return new Response('Failed to load index.html', { status: 500 });
     }
